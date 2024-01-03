76bacc6755784045d39458c1662045698edb7b43
Arjun <arjunsuthar2112@gmail.com>
2024-01-03 18:57:09 +0530

    settings done

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/Usermodel.js";
+import cloudinary from "../configs/cloudinaryConfig.js";
 
 
 export const registerUser = async (req, res) => {
@@ -79,4 +80,43 @@ export const getCurrentUser = async (req, res) => {
             message: error.message,
         })
     }
+}
+
+export const uploadProfileImage = async (req, res)=> {
+    cloudinary.uploader.upload(req.file.path, {
+        folder: "profile-images",
+    }, function (err, result) {
+        if (err) {
+            console.log(err);
+            return res.status(500).json({
+                success: false,
+                message: "Error"
+            })
+        }
+
+        res.status(200).json({
+            success: true,
+            message: "Your Image Uploaded Successfully!",
+            url: result.secure_url
+        })
+    })
+};
+export const updateUser = async (req,res,next)=>{
+    try {
+        const userId = req.params.id;
+        const salt = await bcrypt.genSalt(10);
+        const hashedpassword = await bcrypt.hash(req.body.password,salt);
+        req.body.password = hashedpassword;
+    
+
+        console.log(req.body)
+        
+       const user =await User.findByIdAndUpdate(userId,req.body)
+        if(user) {
+            res.send("user updated successfully")}
+        if(!user) res.send("user not found")
+      } catch (error) {
+        res.send(error)
+      }
+    
 }
